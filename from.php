d3c41733274ef4afcd920a2dec843021ef748267
semmyking <32928355+semmyking@users.noreply.github.com>
2018-04-23 10:03:39 +0200

    Add files via upload

---
+++ from.php
@@ -0,0 +1,17 @@
+
+<h1>Contactformulier</h1>
+<h3>zet hier jouw naam en klas</h3>
+<hr>
+<pre>
+  <form method="post">
+
+    naam: 	 <input type="text" name="name" autofocus>
+
+    email: 	  <input type="email" name="email">
+
+    bericht: 	<input type="text" name="message">
+
+    			    <input type="submit" name='sendContact'>
+  </form>
+</pre>
+<hr>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	// maak dit scipt werkend: zoek de fouten
+
+	include 'views/from.php';
+
+	if (isset($_POST['sendcontact']))
+	{
+		include 'views/response.php';
+	}
+?>
